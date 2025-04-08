01dbe7443b71b93804d476f0cddc245b336a2fe8
Diego-FSP <diegomgplus014@hotmail.com>
2025-04-08 08:54:15 -0300

    Crear bolillero

---
+++ Bolillero.cs
@@ -0,0 +1,29 @@
+namespace BolilleroClass;
+
+public class Bolillero
+{
+    public list<byte> Bolillas;
+    public list<byte> SBolillas;
+    public short NVeces;
+
+    public Bolillero(list<byte> cantidad)
+    {
+        Bolillas=cantidad;
+        NVeces=1;
+    }
+
+    public Bolillero(list<byte> cantidad, short veces)
+    {
+        Bolillas=cantidad;
+        NVeces=veces;
+    }
+
+    public void DevolverBolillas()
+    {
+        if()
+        for(int n=0;n< SBolillas.Contains)
+    }
+
+    public void SacarBolillas()
+    {}
+} 
